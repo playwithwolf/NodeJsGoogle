1a8a11ef03cc70fdce05ea58c457770bd297c81f
play_with_wolf <play_with_wolf@163.com>
2024-12-05 18:04:48 +0800

    test 1

--- google.js
+++ google.js
@@ -7,12 +7,12 @@ const router = express.Router();
 router.post('/auth',  async (req, res) => {
     console.log(req.body);
     console.log(req.body.idToken);
-   // res.send('get users');
-
-
-
-
-
+    const data = {
+        name: 'John Doe',
+        age: 30,
+        email: 'john@example.com'
+      };
+    res.json(data);
 });
 
 module.exports = router;

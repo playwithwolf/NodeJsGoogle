e53a754fbe1e7414bbb9f171c3b0f9ab7b9101ad
play_with_wolf <play_with_wolf@163.com>
2024-12-06 13:10:47 +0800

    test 11

--- demo2.js
+++ demo2.js
@@ -3,14 +3,12 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 
 const myRouter = require('./google.js');
-const myRouter2 = require('./googlecallback.js');
 const app = express();  
 const port = 80;  
 app.use(cors());
 app.use(bodyParser.json());       // 解析JSON格式的请求体
 app.use(bodyParser.urlencoded({ extended: true })); // 解析URL编码的请求体
 app.use(myRouter);
-app.use(myRouter2);
 app.listen(port, () => {  
     console.log('baijie Server running');  
 });  

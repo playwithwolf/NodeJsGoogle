6124d10cc5c93d503b7ca2839e3bcc6d39b5eefd
play_with_wolf <play_with_wolf@163.com>
2025-03-21 16:22:05 +0800

    更新1

--- amazon.js
+++ amazon.js
@@ -8,6 +8,21 @@ router.post('/amazonAuth',  async (req, res) => {
     console.log(req.body);
     console.log(req.body.t3token);
     console.log(req.body.t3userid);
+
+    const url = `https://api.amazon.com/auth/o2/tokeninfo?access_token=${req.body.t3token}`;
+
+    try {
+        const response = await axios.get(url);
+        console.log(response);
+        console.log("---------------1------------");
+        console.log(response.data);
+        console.log("---------------2------------");
+         
+    } catch (error) {
+        console.error('Error fetching token info:', error);
+        res.status(500).json({ error: 'Failed to fetch token info' });
+    }
+
 });
 
 module.exports = router;

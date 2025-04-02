9d95bbc64a92fa703d77e28382aefde917da0b08
play_with_wolf <play_with_wolf@163.com>
2025-04-02 11:52:48 +0800

    1

--- amazon.js
+++ amazon.js
@@ -47,7 +47,7 @@ router.post('/amazonAuth',  async (req, res) => {
               
                   // 使用 Axios 发送 GET 请求
                   const response = await axios.get('https://api.amazon.com/user/profile', { headers });
-                   console.log("response = "+response.data);
+                  console.log("response = "+ JSON.stringify(response.data));
                  // const dataMap = deepClone(response.data);
 
                  console.log("email = "+response.data.email);

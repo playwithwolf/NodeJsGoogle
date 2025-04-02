8320ad7906d8e7f61158948f99b1ee28872cf634
play_with_wolf <play_with_wolf@163.com>
2025-04-02 11:45:39 +0800

    1

--- amazon.js
+++ amazon.js
@@ -25,62 +25,62 @@ router.post('/amazonAuth',  async (req, res) => {
     console.log(req.body.t3userid);
     console.log(req.body.amazonappid);
     
-    // const url = `https://api.amazon.com/auth/o2/tokeninfo?access_token=${req.body.t3token}`;
+    const url = `https://api.amazon.com/auth/o2/tokeninfo?access_token=${req.body.t3token}`;
 
-    // try {
-    //     const response = await axios.get(url);
-    //     //console.log(response);
-    //     console.log("---------------1------------");
-    //     console.log(response.data);
-    //     console.log("---------------2------------");
-    //     console.log("app_id:"+response.data.app_id);
-    //     console.log("req.body.amazonappid:"+ req.body.amazonappid);
-    //     console.log("req.body.t3userid:"+ req.body.t3userid);
-    //     console.log("response.data.user_id:"+ response.data.user_id);
-    //     if(response.data.app_id == req.body.amazonappid && req.body.t3userid == response.data.user_id ){
-    //         console.log("---------------ok------------");
-    //         try{
+    try {
+        const response = await axios.get(url);
+        //console.log(response);
+        console.log("---------------1------------");
+        console.log(response.data);
+        console.log("---------------2------------");
+        console.log("app_id:"+response.data.app_id);
+        console.log("req.body.amazonappid:"+ req.body.amazonappid);
+        console.log("req.body.t3userid:"+ req.body.t3userid);
+        console.log("response.data.user_id:"+ response.data.user_id);
+        if(response.data.app_id == req.body.amazonappid && req.body.t3userid == response.data.user_id ){
+            console.log("---------------ok------------");
+            try{
 
-    //             const headers = {
-    //                 'Authorization': `bearer ${req.body.t3token}`
-    //               };
+                const headers = {
+                    'Authorization': `bearer ${req.body.t3token}`
+                  };
               
-    //               // 使用 Axios 发送 GET 请求
-    //               const response = await axios.get('https://api.amazon.com/user/profile', { headers });
-    //                console.log("response = "+response.data);
-    //              // const dataMap = deepClone(response.data);
+                  // 使用 Axios 发送 GET 请求
+                  const response = await axios.get('https://api.amazon.com/user/profile', { headers });
+                   console.log("response = "+response.data);
+                 // const dataMap = deepClone(response.data);
 
-    //              console.log("email = "+response.data.email);
-    //               // 返回响应数据
-    //               //res.json(response);
+                 console.log("email = "+response.data.email);
+                  // 返回响应数据
+                  //res.json(response);
 
-    //         }catch(error){
-    //             console.error('profile Error:', error);
-    //         }
-    //     }
+            }catch(error){
+                console.error('profile Error:', error);
+            }
+        }
          
-    // } catch (error) {
-    //     console.error('Error fetching token info:', error);
-    //     res.status(500).json({ error: 'Failed to fetch token info' });
-    // }
-    try{
+    } catch (error) {
+        console.error('Error fetching token info:', error);
+        res.status(500).json({ error: 'Failed to fetch token info' });
+    }
+  //   try{
 
-      const headers = {
-          'Authorization': `bearer ${req.body.t3token}`
-        };
+  //     const headers = {
+  //         'Authorization': `bearer ${req.body.t3token}`
+  //       };
     
-        // 使用 Axios 发送 GET 请求
-        const response = await axios.get('https://api.amazon.com/user/profile', { headers });
-         console.log("response = "+ JSON.stringify(response.data));
-       // const dataMap = deepClone(response.data);
+  //       // 使用 Axios 发送 GET 请求
+  //       const response = await axios.get('https://api.amazon.com/user/profile', { headers });
+  //        console.log("response = "+ JSON.stringify(response.data));
+  //      // const dataMap = deepClone(response.data);
 
-       console.log("email = "+response.data.email);
-        // 返回响应数据
-        //res.json(response);
+  //      console.log("email = "+response.data.email);
+  //       // 返回响应数据
+  //       //res.json(response);
 
-  }catch(error){
-      console.error('profile Error:', error);
-  }
+  // }catch(error){
+  //     console.error('profile Error:', error);
+  // }
 
 });
 
